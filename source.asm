046ed7490d9c18883963c2ad87004dd94d68ac3b
allanh610 <168477166+allanh610@users.noreply.github.com>
2024-12-04 14:06:23 -0500

    Create source.asm
    Beginning the overall project with the first 5 lines. These lines define messages and formatting strings that the program will use when interacting with the user or displaying results

---
+++ source.asm
@@ -0,0 +1,5 @@
+.data
+prompt: .asciiz "Enter a number (that's greater than or equal to 25): "
+invalid_msg: .asciiz "Illegal Number!\n"
+fib_msg: .asciiz "Fibonacci sequence: \n"
+comma: .asciiz ",'
