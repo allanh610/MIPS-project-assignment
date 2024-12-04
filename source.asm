bd8df69d3213693f7ed18f2ff3d21e1e72f945a8
allanh610 <168477166+allanh610@users.noreply.github.com>
2024-12-04 14:26:49 -0500

    Update source.asm
    Code that defines the beginning of the executable instructions for the program. The print_string subroutine is designed to print strings.

--- source.asm
+++ source.asm
@@ -3,3 +3,10 @@ prompt: .asciiz "Enter a number (that's greater than or equal to 25): "
 invalid_msg: .asciiz "Illegal Number!\n"
 fib_msg: .asciiz "Fibonacci sequence: \n"
 comma: .asciiz ",'
+
+.text
+.global main
+
+# Prints a string
+print_string:
+    li $v0, 4
