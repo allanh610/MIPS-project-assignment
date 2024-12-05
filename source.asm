d394505a202d0a562bc3086477a5634bc055e44b
allanh610 <168477166+allanh610@users.noreply.github.com>
2024-12-05 12:45:04 -0500

    Update source.asm
    Continues coding for fib_loop function

--- source.asm
+++ source.asm
@@ -66,3 +66,12 @@ fibonacci_sequence:   # Prints the first numbers in the Fibonacci sequence
 
 fib_loop:
     bge $t4, $t0, end_fib
+
+    add $t5, $t2, $t3
+
+    la $a0, comma
+    jal print_string
+
+    li $v0, 1
+    move $a0, $t5
+    syscall
